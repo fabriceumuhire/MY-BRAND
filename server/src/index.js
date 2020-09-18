d3443f3b4fb29b761f846ab23331f238d04f10e1
Merci23 <wayjorci@gmail.com>
2020-09-18 04:05:01 +0200

    Adding .gitignore for node modules, .env and tmp

--- server/src/index.js
+++ server/src/index.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const articles = require("./routes/articles");
-const fileupload = require("express-fileupload");
+const queries = require("./routes/queries");
 
 
 mongoose
-  .connect("mongodb://localhost:27017/blogdb", { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect("mongodb://localhost:27017/blogdb", { useNewUrlParser: true })
   .then(() => {
     const app = express();
     app.use(express.json());
-    app.use(fileupload({
-        createParentPath: true,
-        useTempFiles: true
-    }))
-    app.use("/api/routes", articles);
+    app.use("/api/routes", queries);
 
     app.listen(5000, () => {
       console.log("Server has started!");
