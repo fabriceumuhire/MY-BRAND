35cb589de87627730aad554883e0a92e383c3da3
Merci23 <wayjorci@gmail.com>
2020-11-04 03:01:44 +0200

    Contact form queries with validation

--- UI/js/app.js
+++ UI/js/app.js
@@ -1,12 +1,12 @@
 // Visit https://console.firebase.google.com/project/<your project id>/overview and click on "Add Firebase to your web app"
 const firebaseConfig = {
-    apiKey: "AIzaSyBIRRiFPxqQ1dRhjIAg0gzzQo7PfYCuSo8",
-    authDomain: "tech-u-blog.firebaseapp.com",
-    databaseURL: "https://tech-u-blog.firebaseio.com",
-    projectId: "tech-u-blog",
-    storageBucket: "tech-u-blog.appspot.com",
-    messagingSenderId: "1029044573579",
-    appId: "1:1029044573579:web:cfa5c2820cedb2dbb94b8e",
-    measurementId: "G-FGBVGDH3YL"
-};
-firebase.initializeApp(firebaseConfig);
+    apiKey: "AIzaSyAmFBKq3R-hNqyAXWZk-YWJrNY-i-hh-rY",
+    authDomain: "techuproj.firebaseapp.com",
+    databaseURL: "https://techuproj.firebaseio.com",
+    projectId: "techuproj",
+    storageBucket: "techuproj.appspot.com",
+    messagingSenderId: "566434324120",
+    appId: "1:566434324120:web:fe2b28c1aa930e369efaed"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
